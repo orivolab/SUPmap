de7833aa0a321f4c4a0b6fcba48c1cde9dc064ca
Emilia Dudziak <emiliadudziak@MacBook-Air-Emilia.local>
2026-08-07 12:44:33 +0200

    Naprawa ikony pinezki na mapie wyboru lokalizacji

--- src/components/LocationPicker.jsx
+++ src/components/LocationPicker.jsx
@@ -1,4 +1,17 @@
 import { Marker, Popup, useMapEvents } from "react-leaflet";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const marker = L.icon({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
 
 function LocationPicker({ position, onSelect }) {
   useMapEvents({
@@ -12,7 +25,7 @@ function LocationPicker({ position, onSelect }) {
   }
 
   return (
-    <Marker position={[position.lat, position.lng]}>
+    <Marker position={[position.lat, position.lng]} icon={marker}>
       <Popup>Wybrana lokalizacja</Popup>
     </Marker>
   );
